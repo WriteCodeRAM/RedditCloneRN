b125095471e16f1982990013b7b97ab739224243
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2025-10-15 13:07:18 -0400

    feat: added types and FlatList to display posts

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,9 +1,14 @@
-import { Text, View } from "react-native";
+import { View, FlatList } from "react-native";
+import posts from "../../../assets/data/post.json";
+import PostListItem from "../../components/PostListItem";
 
 export default function HomeScreen() {
   return (
     <View>
-      <Text>Home Screen</Text>
+      <FlatList
+        data={posts}
+        renderItem={({ item }) => <PostListItem post={item} />}
+      />
     </View>
   );
 }
